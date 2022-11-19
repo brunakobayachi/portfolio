1875526cf0cec242cd011a752feb47031126cf3e
brunakobayachi <brunakobayachi@gmail.com>
2022-11-18 23:26:31 -0300

    feat setting initial configs

---
+++ frontend_react/src/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./App.scss";
+import { About, Footer, Header, Skills, Testimonial, Work } from "./container";
+import { Navbar } from "./components";
+const App = () => {
+    return (
+        <div className="app">
+            <Navbar />
+            <Header />
+            <About />
+            <Work />
+            <Skills />
+            <Testimonial />
+            <Footer />
+        </div>
+    );
+};
+
+export default App;
---
+++ frontend_react/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { images } from "../../constants";
+import "./Navbar.scss";
+
+const Navbar = () => {
+    return (
+        <nav className="app__navbar">
+            <div className="app__navbar-logo">
+                <img src={images.logo} alt="" />
+            </div>
+            <ul className="app__navbar-links">
+                {["home", "about", "work", "skills", "contact"].map(item => (
+                    <li className="app__flex p-text" key={`link-${item}`}>
+                        <div/>
+                        <a href={`#${item}`}>{item}</a>
+                    </li>
+                ))}
+            </ul>
+        </nav>
+    );
+};
+
+export default Navbar;
